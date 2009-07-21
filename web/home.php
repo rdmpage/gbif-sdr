913a790f3f0ce784943dafe2eb55de29e9771efa
fmr1983 <fmr1983@ee5e1b7c-6bb1-11de-bf08-17995f091a54>
2009-07-21 13:57:02 +0000

    problems with header & footer

--- web/home.php
+++ web/home.php
@@ -6,7 +6,5 @@ require 'services/SDRServices.php';
 $smarty = new Smarty;
 $services = new SDRServices;
 
-//$data =$services->getItemList(10);
-//$smarty->assign('data', $data);
 $smarty->display('home.tpl');
 ?>
